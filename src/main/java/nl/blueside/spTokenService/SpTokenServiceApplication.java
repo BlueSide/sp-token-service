041e91d91e2cd94553fef6dcaded69050b40c143
cikzh <marlon@blueside.nl>
2018-09-11 13:44:03 +0200

    Initial commit

---
+++ src/main/java/nl/blueside/spTokenService/SpTokenServiceApplication.java
@@ -0,0 +1,12 @@
+package nl.blueside.spTokenService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpTokenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpTokenServiceApplication.class, args);
+	}
+}
